926e5d9851390d1aad15827b68cbc69873f69565
Shiro <63391697+ShiroGast@users.noreply.github.com>
2022-09-26 11:28:45 +0200

    Finall commit

--- index.php
+++ index.php
@@ -4,17 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
     <title>Open dag</title>
 </head>
 <body>
     <header>
-
+        <img src="img/logo.png" alt="logo">
+        <!-- <p>Gemaakt door: Shiro Gast, 2de jaars</p> -->
     </header>
     <main>
-        
+        <div class="welcome">
+            <img class="welcome_img" src="img/welcome.png" alt="welcome">
+            <div class="welcome_text">
+                <h1>Welkom bij Curio!</h1>
+                <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia corporis dolores doloribus earum! Error labore, voluptatem asperiores iusto quaerat voluptas ducimus commodi quas corrupti magnam dicta nisi voluptatibus eligendi repellendus!</h2>
+            </div>
+        </div>
+        <div class="green">
+            <h1>Vakken</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquam cum, quam sed fuga ipsam veritatis dignissimos consequatur. Facilis cupiditate deleniti non magni, voluptate quis numquam in sapiente laborum aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quae, dignissimos quis laudantium est nisi asperiores voluptates veritatis blanditiis culpa vel? Quibusdam quas quis illo dolore magnam aut labore reiciendis.</p>
+            <img src="img/classes.png" alt="img1">
+        </div>
+        <div class="blue">
+            <h1>Rooster eerstejaars</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquam cum, quam sed fuga ipsam veritatis dignissimos consequatur. Facilis cupiditate deleniti non magni, voluptate quis numquam in sapiente laborum aspernatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolorum dolorem laborum eius facilis repudiandae harum blanditiis veniam quisquam libero quis nobis sint ut cum quos, neque ipsam, quas reiciendis.</p>
+            <img src="img/schedule.png" alt="img2">
+        </div>
+        <div class="green">
+            <h1>Indeling van blokken & versnellen</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquam cum, quam sed fuga ipsam veritatis dignissimos consequatur. Facilis cupiditate deleniti non magni, voluptate quis numquam in sapiente laborum aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt veritatis saepe quasi dolorem delectus? Quaerat voluptatibus maxime assumenda molestias deserunt mollitia quia odio exercitationem explicabo, pariatur nam sit, eum dicta.</p>
+            <img src="img/overview.png" alt="img3">
+        </div>
+        <div class="face_book">
+            <h1>Smoelen boek</h1>
+            <div class="teachers">
+                <div class="teacher">
+                    <img src="img/teachers/fedde.png" alt="teacher1">
+                    <h2>Fedde van Gils</h2>
+                </div>
+                <div class="teacher">
+                    <img src="img/teachers/ine.png" alt="teacher2">
+                    <h2>Ine Vermeer</h2>
+                </div>
+                <div class="teacher">
+                    <img src="img/teachers/joost.png" alt="teacher3">
+                    <h2>Joost Ambagts</h2>
+                </div>
+            </div>
+        </div>
     </main>
     <footer>
-
+        <p>&#169;Curio</p>
     </footer>
 </body>
 </html>
